1b33524ca826ec7a14e1cff6dd6cbf38cedc9a9c
Benoit Tavernier <benoit.tavernier@gmail.com>
2025-10-27 12:21:26 +0100

    Exercice sur les tableaux

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+
+$users = require 'users.php';
+
+var_dump($users);
---
+++ users.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'username' => 'benoit',
+        'password' => 'benoit',
+        'roles' => ['ROLE_USER', 'ROLE_ADMIN'],
+        'creation_date' => '2025-10-27'
+    ],
+    [
+        'id' => 2,
+        'username' => 'cyrille',
+        'password' => 'cyrille',
+        'roles' => ['ROLE_USER'],
+        'creation_date' => '2025-10-27'
+    ],
+    [
+        'id' => 3,
+        'username' => 'manon',
+        'password' => 'manon',
+        'roles' => ['ROLE_USER'],
+        'creation_date' => '2025-10-27'
+    ],
+];
