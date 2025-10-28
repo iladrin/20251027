030d185abb06fc5530bd4c8b9f185d2c50cfa0e9
Benoit Tavernier <benoit.tavernier@gmail.com>
2025-10-28 17:00:45 +0100

    PHPCS : standard de codage

--- app/templates/users/list.html.php
+++ app/templates/users/list.html.php
@@ -1,19 +1,19 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Liste des utilisateurs</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Liste des utilisateurs</title>
 </head>
 <body>
 <h1>Liste des utilisateurs</h1>
 
 <ul>
-  <?php
-  foreach ($users as $user) {
-    echo '<li><a href="?page=user_profile&username=' . $user['username'] . '">' . $user['username'] . '</a></li>';
-  }
-  ?>
+    <?php
+    foreach ($users as $user) {
+        echo '<li><a href="?page=user_profile&username=' . $user['username'] . '">' . $user['username'] . '</a></li>';
+    }
+    ?>
 </ul>
 
 </body>
