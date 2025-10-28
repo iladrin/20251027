5c38eda25f2b88902e4d839ff0fc67f2bd060d7e
Benoit Tavernier <benoit.tavernier@gmail.com>
2025-10-28 12:06:36 +0100

    Utilisation d'un fichier de routage

---
+++ config/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'users' => 'users/list.php',
+    'user_list' => 'users/list.php',    // Alias de la route précédente
+
+    'user_profile' => 'users/show.php',
+    'homepage' => 'homepage.php',
+];
--- public/index.php
+++ public/index.php
@@ -9,18 +9,13 @@ const APP_DIR = PROJECT_DIR . '/app';
 const CONTROLLERS_DIR = APP_DIR . '/controllers';
 const VIEWS_DIR = APP_DIR . '/templates';
 const MODELS_DIR = APP_DIR . '/data';
+const CONFIG_DIR = PROJECT_DIR . '/config';
 
 // Par exemple: ?page=user_profile
 $page = $_GET['page'] ?? 'homepage';
 
-// @todo Définir les routes dans un fichier de configuration. Merci :}
-$routes = [
-    'users' => 'users/list.php',
-    'user_list' => 'users/list.php',    // Alias de la route précédente
-
-    'user_profile' => 'users/show.php',
-    'homepage' => 'homepage.php',
-];
+// Chargement des routes connues par l’application
+$routes = require CONFIG_DIR . '/routes.php';
 
 // On vérifie que l'index $page (par exemple 'user_profile') existe dans le tableau $routes
 if (!array_key_exists($page, $routes)) {
